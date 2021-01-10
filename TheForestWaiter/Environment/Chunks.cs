51cc1c8bfe6e40a045ee967a3443436f436e1243
Dennis <dennis.com>
2021-01-10 13:21:49 +0100

    Added chunks class

---
+++ TheForestWaiter/Environment/Chunks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForestWaiter.Environment
+{
+    class Chunks
+    {
+        public const float CHUNK_WIDTH = 100;
+        public const float RENDER_DISTANCE = 2000;
+
+        private World World { get; set; }
+        public Chunks(World world)
+        {
+
+        }
+    }
+}
