54fe1ef67b272f08d6acdb0dd9e1b20c72132a6b
Dennis <50838791+den-nis@users.noreply.github.com>
2021-10-12 19:50:38 +0200

    Improved spawners

---
+++ TheForestWaiter/Game/Essentials/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForestWaiter.Game.Essentials
+{
+	public class Timer
+	{
+		private float _interval = 1;
+		public bool Enabled { get; set; } = true;
+
+		public TimeSpan Interval => TimeSpan.FromSeconds(_interval);
+		private float _time;
+
+		public event Action OnTick;
+
+		public Timer(float interval) => SetInterval(interval);
+		
+		public Timer(TimeSpan interval) => SetInterval(interval);
+		
+		public Timer()
+		{
+			
+		}
+
+		public void SetInterval(float interval)
+		{
+			_interval = interval;
+		}
+
+		public void SetInterval(TimeSpan interval)
+		{
+			_interval = (float)interval.TotalSeconds;
+		}
+
+		public void Update(float time)
+		{
+			if (OnTick != null && Enabled)
+			{
+				_time += time;
+
+				while (_time > _interval)
+				{
+					_time -= _interval;
+					OnTick?.Invoke();
+				}
+			}
+		}
+	}
+}
--- TheForestWaiter/Game/Objects/Static/Spawner.cs
+++ TheForestWaiter/Game/Objects/Static/Spawner.cs
@@ -1,4 +1,5 @@
 ﻿using SFML.Graphics;
+using SFML.System;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,20 +7,30 @@ using System.Text;
 using System.Threading.Tasks;
 using TheForestWaiter.Game.Entities;
 using TheForestWaiter.Game.Environment;
+using TheForestWaiter.Game.Essentials;
 
 namespace TheForestWaiter.Game.Objects.Static
 {
     class Spawner : StaticObject
     {
-        private readonly ObjectCreator _creator;
+		private readonly GameData _game;
+		private readonly ObjectCreator _creator;
         private bool _initialUpdate = false;
         private int _amount;
-        private int _interval;
+
+        /// <summary>
+        /// 0 = Anywhere 
+        /// </summary>
+        private Vector2i _triggerDistance = new(0,0);
+
+        private readonly Timer _timer = new();
         private Type _spawnType;
 
         public Spawner(GameData game, ObjectCreator creator) : base(game)
         {
-            _creator = creator;
+			_game = game;
+			_creator = creator;
+            _timer.OnTick += Spawn;
         }
 
         public override void Draw(RenderWindow window)
@@ -28,26 +39,28 @@ namespace TheForestWaiter.Game.Objects.Static
 
         public override void Update(float time)
         {
+            _timer.Update(time);
+
             if (!_initialUpdate)
             {
-                OnFirstUpdate();
-            }
+                Spawn();
+			}
 
-            _initialUpdate = true;
-        }
-
-        private void OnFirstUpdate()
-        {
-            Spawn();
+			_initialUpdate = true;
         }
 
         private void Spawn()
         {
-            for (int i = 0; i < _amount; i++)
+            var distance = (_game.Objects.Player.Center - Center).Abs();
+
+            if ((distance.X < _triggerDistance.X || _triggerDistance.X == 0f) && (distance.Y < _triggerDistance.Y || _triggerDistance.Y == 0))
             {
-                var obj = _creator.CreateType(_spawnType);
-                obj.Position = Position;
-                Game.Objects.AddAuto(obj);
+                for (int i = 0; i < _amount; i++)
+                {
+                    var obj = _creator.CreateType(_spawnType);
+                    obj.Position = Position;
+                    Game.Objects.AddAuto(obj);
+                }
             }
         }
 
@@ -55,10 +68,22 @@ namespace TheForestWaiter.Game.Objects.Static
         {
             var lookup = mapObject.Properties.ToDictionary(k => k.Name, v => v.Value);
             _amount = int.Parse(lookup["Amount"]);
-            _interval = int.Parse(lookup["Interval"]);
-            var name = lookup["Object"];
+            
+            var triggerDistanceParts = lookup["TriggerDistance"].Split(',');
+            _triggerDistance = new(
+                int.Parse(triggerDistanceParts[0]) * World.TILE_SIZE, 
+                int.Parse(triggerDistanceParts[1]) * World.TILE_SIZE
+                );
 
-            Types.GameObjects.TryGetValue(name, out _spawnType);
+            float interval = float.Parse(lookup["Interval"]);
+
+            if (interval != 0.0f)
+            {
+                _timer.SetInterval(interval);
+                _timer.OnTick += Spawn;
+            }
+
+            Types.GameObjects.TryGetValue(lookup["Object"], out _spawnType);
         }
     }
 }
