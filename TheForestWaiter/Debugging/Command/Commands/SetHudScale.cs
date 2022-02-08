55fe8c83640c06e11558d35518425d3e58da7613
Dennis <50838791+den-nis@users.noreply.github.com>
2022-02-08 13:21:50 -0800

    Command improvements

---
+++ TheForestWaiter/Debugging/Command/Commands/SetHudScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheForestWaiter.Game.Hud;
+
+namespace TheForestWaiter.Debugging.Command.Commands
+{
+    [Command("sethudscale", "", "{number}")]
+    class SetHudScale : ICommand
+    {
+        private readonly HudDrawer _hud;
+
+        public SetHudScale(HudDrawer hud)
+        {
+            _hud = hud;
+        }
+
+        public void Execute(CommandHandler handler, string[] args)
+        {
+            _hud.HudScale = float.Parse(args[0]);
+        }
+    }
+}
---
+++ TheForestWaiter/Debugging/Command/Commands/Setpos.cs
@@ -0,0 +1,22 @@
+﻿using SFML.System;
+using System;
+using TheForestWaiter.Game;
+
+namespace TheForestWaiter.Debugging.Command.Commands
+{
+    [Command("setpos", "Set the position of the player", "{x} {y}")]
+    class SetPos : ICommand
+    {
+        private readonly GameData _game;
+
+        public SetPos(GameData game)
+        {
+            _game = game;
+        }
+
+        public void Execute(CommandHandler handler, string[] args)
+        {
+            _game.Objects.Player.Position = new Vector2f(float.Parse(args[0]), float.Parse(args[1]));
+        }
+    }
+}
