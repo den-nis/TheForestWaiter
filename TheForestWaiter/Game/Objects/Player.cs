8c43c2b6439cf50aa2604e3675880069e660c587
Dennis <50838791+den-nis@users.noreply.github.com>
2022-02-10 11:50:55 -0800

    Player refactor

--- TheForestWaiter/Game/Objects/Player.cs
+++ TheForestWaiter/Game/Objects/Player.cs
@@ -15,35 +15,34 @@ using TheForestWaiter.Game.Objects.Weapons.Guns;
 
 namespace TheForestWaiter.Game.Objects
 {
-    class Player : Creature
+    internal class Player : Creature
     {
-        private const float SPEED_UP_PER_SECOND = 50;
-        private const float SECONDS_OF_SPEED_UP = 4;
-        private const float MAX_NORMAL_SPEED = 200;
+        private const float JUMP_VELOCITY = 380;
+        private const float WALK_SPEED = 260;
+        private const float EARLY_JUMP_TIME = 100 * Time.MILLISECONDS;
 
-        private readonly Color _stunColor = new(255,200,200);
-        private readonly Camera _camera;
-
-        public AnimatedSprite AnimatedSprite { get; set; }
         public GunBase Gun { get; private set; }
 
-        private float EarlyJumpTime { get; set; } = (float)TimeSpan.FromSeconds(0.1f).TotalSeconds;
-        private float EarlyJumpTimer { get; set; }
+        private readonly AnimatedSprite _animatedSprite;
+        private readonly Color _stunColor = new(255, 200, 200);
+        private readonly Camera _camera;
 
-        private bool Jumping { get; set; } = false;
-        private bool MoveRight { get; set; } = false;
-        private bool MoveLeft { get; set; } = false;
-        private Vector2f _aim;
+        private float _earlyJumpTimer;
+
+        private bool _jumping = false;
+        private bool _moveRight = false;
+        private bool _moveLeft = false;
         private bool _aimingRight = true;
-        private float _speedBuildUp = 0;
+
+        private Vector2f _aim;
 
         public Player(GameData game, ContentSource content, ObjectCreator creator, Camera camera) : base(game)
         {
             Health = 100;
-            AnimatedSprite = content.Textures.CreateAnimatedSprite("Textures\\Player\\sheet.png");
+            _animatedSprite = content.Textures.CreateAnimatedSprite("Textures\\Player\\sheet.png");
             Gun = creator.CreateGun<Handgun>();
 
-            Size = AnimatedSprite.Sheet.TileSize.ToVector2f();
+            Size = _animatedSprite.Sheet.TileSize.ToVector2f();
             CollisionRadius = Size.Y + 5;
             ReceivePhysicsCollisions = true;
             EmitPhysicsCollisions = false;
@@ -55,7 +54,7 @@ namespace TheForestWaiter.Game.Objects
             if (!Dead)
             {
                 Gun?.Draw(window);
-                window.Draw(AnimatedSprite.Sheet.Sprite);
+                window.Draw(_animatedSprite.Sheet.Sprite);
             }
         }
 
@@ -68,7 +67,7 @@ namespace TheForestWaiter.Game.Objects
         public void StopFire()
         {
             if (Gun != null)
-			    Gun.Firing = false;
+                Gun.Firing = false;
         }
 
         public void Aim(Vector2f mouse)
@@ -78,18 +77,18 @@ namespace TheForestWaiter.Game.Objects
             Gun?.Aim(mouse);
         }
 
-        public void StartMoveRight() => MoveRight = true;
-        public void StopMoveRight() => MoveRight = false;
-        public void StartMoveLeft() => MoveLeft = true;
-        public void StopMoveLeft() => MoveLeft = false;
+        public void StartMoveRight() => _moveRight = true;
+        public void StopMoveRight() => _moveRight = false;
+        public void StartMoveLeft() => _moveLeft = true;
+        public void StopMoveLeft() => _moveLeft = false;
 
         public void StartJump()
         {
-            Jumping = true;
-            EarlyJumpTimer = EarlyJumpTime;
+            _jumping = true;
+            _earlyJumpTimer = EARLY_JUMP_TIME;
         }
 
-        public void StopJump() => Jumping = false;
+        public void StopJump() => _jumping = false;
 
         private void HandleMovement(float time)
         {
@@ -101,96 +100,81 @@ namespace TheForestWaiter.Game.Objects
 
             if (TouchingFloor)
             {
-                HandleSpeedBuildUp(time);
-
-                if (EarlyJumpTimer > 0)
-                    SetVelocityY(-350);
+                if (_earlyJumpTimer > 0)
+                    SetVelocityY(-JUMP_VELOCITY);
             }
-            EarlyJumpTimer -= time;
+            _earlyJumpTimer -= time;
 
-            if (Jumping)
+            if (_jumping)
                 Push(new Vector2f(0, -300), time);
 
-            if (MoveRight && !MoveLeft)
-                LimitPush(new Vector2f(MAX_NORMAL_SPEED + _speedBuildUp * SPEED_UP_PER_SECOND, 0), 1);
+            if (_moveRight && !_moveLeft)
+                LimitPush(new Vector2f(WALK_SPEED, 0), 1);
 
-            if (MoveLeft && !MoveRight)
-                LimitPush(new Vector2f(-MAX_NORMAL_SPEED + _speedBuildUp * SPEED_UP_PER_SECOND, 0), 1);
+            if (_moveLeft && !_moveRight)
+                LimitPush(new Vector2f(-WALK_SPEED, 0), 1);
 
-            EnableDrag = !MoveLeft && !MoveRight;
-        }
-
-        private void HandleSpeedBuildUp(float time)
-		{
-            if (RealSpeed.X > -1 && RealSpeed.X < 1)
-                _speedBuildUp = 0;
-
-            if (RealSpeed.X > 1 || MoveRight)
-                _speedBuildUp = Math.Min(SECONDS_OF_SPEED_UP, Math.Max(0, _speedBuildUp + time));
-            
-            if (RealSpeed.X < -1 || MoveLeft)
-                _speedBuildUp = Math.Max(-SECONDS_OF_SPEED_UP, Math.Min(0, _speedBuildUp - time));
-
+            EnableDrag = !_moveLeft && !_moveRight;
         }
 
         private void HandleAnimations(float time)
         {
             if (IsStunned)
             {
-                AnimatedSprite.Sprite.Color = _stunColor;
+                _animatedSprite.Sprite.Color = _stunColor;
                 if (Gun != null) Gun.GunSprite.Color = _stunColor;
             }
             else
             {
                 if (Gun != null) Gun.GunSprite.Color = Color.White;
-                AnimatedSprite.Sprite.Color = Color.White;
+                _animatedSprite.Sprite.Color = Color.White;
             }
 
             var isMovingRight = RealSpeed.X > 1;
             var isMovingLeft = RealSpeed.X < -1;
 
-            AnimatedSprite.Sheet.MirrorX = !_aimingRight;
-            AnimatedSprite.Framerate = 12;
+            _animatedSprite.Sheet.MirrorX = !_aimingRight;
+            _animatedSprite.Framerate = 12;
 
             if ((_aimingRight && isMovingRight) || (!_aimingRight && isMovingLeft))
             {
                 //Forward walking
-                AnimatedSprite.AnimationStart = 0;
-                AnimatedSprite.AnimationEnd = 6;
+                _animatedSprite.AnimationStart = 0;
+                _animatedSprite.AnimationEnd = 6;
             }
             else
             {
                 //Backward walking
-                AnimatedSprite.AnimationStart = 8;
-                AnimatedSprite.AnimationEnd = 15;
+                _animatedSprite.AnimationStart = 8;
+                _animatedSprite.AnimationEnd = 15;
             }
-            
+
             if (Math.Abs(RealSpeed.X) < 1f)
             {
                 //Idle
-                AnimatedSprite.Framerate = 5;
-                AnimatedSprite.AnimationStart = 14;
-                AnimatedSprite.AnimationEnd = 19;
+                _animatedSprite.Framerate = 5;
+                _animatedSprite.AnimationStart = 14;
+                _animatedSprite.AnimationEnd = 19;
             }
 
             if (!TouchingFloor)
-                AnimatedSprite.SetStaticFrame(3);
-            
-            AnimatedSprite.Sprite.Position = Position;
-            AnimatedSprite.Update(time);
+                _animatedSprite.SetStaticFrame(3);
+
+            _animatedSprite.Sprite.Position = Position;
+            _animatedSprite.Update(time);
         }
 
         public override void Update(float time)
         {
-			HandleMovement(time);
             PhysicsTick(time);
+            HandleMovement(time);
 
             //Gun needs to updated after physics tick since it needs the latest player position
             Gun?.Update(time);
             HandleAnimations(time);
             base.Update(time);
         }
-         
+
         public void RemoveGun() => SetGun(null);
 
         public void SetGun(GunBase gun)
@@ -198,7 +182,7 @@ namespace TheForestWaiter.Game.Objects
             if (gun != null)
             {
                 gun.Aim(_aim);
-			}
+            }
 
             Gun = gun;
         }
@@ -211,7 +195,7 @@ namespace TheForestWaiter.Game.Objects
 
         protected override void OnDamage(PhysicsObject by)
         {
-            if (by != null) 
+            if (by != null)
                 ApplyStunMovement(by);
         }
     }
---
+++ TheForestWaiter/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForestWaiter
+{
+	/// <summary>
+	/// Simple list of constant for converting to seconds
+	/// </summary>
+	internal static class Time
+	{
+		public const float MILLISECONDS = 0.001f;
+		public const float HOURS = MINUTES * 60;
+		public const float MINUTES = 60;
+	}
+}
