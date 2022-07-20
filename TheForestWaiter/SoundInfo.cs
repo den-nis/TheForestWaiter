ce1596bfc7b8c03a640982942d3562352f8eb573
Dennis <50838791+den-nis@users.noreply.github.com>
2022-07-20 20:26:54 +0200

    Refactor

---
+++ TheForestWaiter/SoundInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace TheForestWaiter
+{
+	internal record SoundInfo
+	{
+		public string Identifier { get; }
+		public float Volume { get; set; } = 50;
+		public float Pitch { get; set; } = 1;
+
+		public float PitchVariation { get; set; }
+		public float VolumeVariation { get; set; }
+
+		private bool _none = false;
+
+		public static SoundInfo None
+		{
+			get
+			{
+				var sound = new SoundInfo(string.Empty);
+				sound._none = true;
+				return sound;
+			}
+		}
+
+		public bool IsSilent() => _none;
+
+		public SoundInfo(string identifier)
+		{
+			Identifier = identifier;
+		}
+	}
+}
