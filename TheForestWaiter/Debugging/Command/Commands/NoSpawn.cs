1f3b57c7170993223b11d320861c9bbe1eedd4f2
Dennis <50838791+den-nis@users.noreply.github.com>
2022-07-22 23:16:37 +0200

    Added disable spawner command

---
+++ TheForestWaiter/Debugging/Command/Commands/NoSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using TheForestWaiter.Game;
+using TheForestWaiter.Game.Objects.Static;
+
+namespace TheForestWaiter.Debugging.Command.Commands
+{
+	[Command("nospawn", "Delete all enemies and disable future spawning")]
+	internal class NoSpawn : ICommand
+	{
+		private readonly GameData _gameData;
+
+		public NoSpawn(GameData gameData)
+		{
+			_gameData = gameData;
+		}
+
+		public void Execute(CommandHandler handler, string[] args)
+		{
+			foreach(var creature in _gameData.Objects.Creatures.Where(c => !c.Friendly))
+			{
+				Console.WriteLine($"Deleted {creature.GetType().Name} ({creature.GameObjectId})");
+				creature.Delete();
+			}
+
+			_gameData.Objects.Environment.FirstOrDefault(o => o is Spawner)?.Delete();
+		}
+	}
+}
