e3071b90a63e85c387be2fe4e39f608998cd7411
Dennis <50838791+den-nis@users.noreply.github.com>
2022-08-02 16:53:18 +0200

    Kill command

---
+++ TheForestWaiter/Debugging/Command/Commands/Kill.cs
@@ -0,0 +1,21 @@
+﻿using TheForestWaiter.Game;
+
+namespace TheForestWaiter.Debugging.Command.Commands
+{
+	[Command("kill", "Kill the player")]
+	internal class Kill : ICommand
+	{
+		private readonly GameData _gameData;
+
+		public Kill(GameData gameData)
+		{
+			_gameData = gameData;
+		}
+
+		public void Execute(CommandHandler handler, string[] args)
+		{
+			var player = _gameData.Objects.Player;
+			player.Damage(null, 9001, 0);
+		}
+	}
+}
