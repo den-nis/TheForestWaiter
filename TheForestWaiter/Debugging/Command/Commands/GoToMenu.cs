42807d88078a4071c0000512c42195868871fc55
Dennis <50838791+den-nis@users.noreply.github.com>
2022-08-02 13:15:35 +0200

    Go to menu command

---
+++ TheForestWaiter/Debugging/Command/Commands/GoToMenu.cs
@@ -0,0 +1,28 @@
+﻿using LightInject;
+using TheForestWaiter.UI.Menus;
+using TheForestWaiter.States;
+
+namespace TheForestWaiter.Debugging.Command.Commands
+{
+	[Command("menu", "Go to the menu", "{name}")]
+	internal class GoToMenu : ICommand
+	{
+		private readonly StateManager _stateManager;
+		private readonly IServiceContainer _container;
+
+		public GoToMenu(StateManager stateManager, IServiceContainer container)
+		{
+			_stateManager = stateManager;
+			_container = container;
+		}
+
+		public void Execute(CommandHandler handler, string[] args)
+		{
+			_stateManager.StartTransition(new StateTransition
+			{
+				TargetState = typeof(MainMenu),
+				Length = 1,
+			}, true);
+		}
+	}
+}
